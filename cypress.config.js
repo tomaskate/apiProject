be292a64f196f18d800db5bddcb1cf87e063276d
tomaskate <workkateryna12370@gmail.com>
2023-10-09 16:36:14 +0300

    some fixes added and script is run

--- cypress.config.js
+++ cypress.config.js
@@ -19,5 +19,6 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    specPattern: "cypress/e2e/**/*.spec.{js,jsx,ts,tsx}",
   },
 });
