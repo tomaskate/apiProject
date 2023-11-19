53d4aaa25b35dac133bb05f3b0ce2bc4b8f3a337
tomaskate <workkateryna12370@gmail.com>
2023-11-19 13:25:05 +0200

    HW 14 test branch

--- cypress.config.js
+++ cypress.config.js
@@ -21,7 +21,7 @@ export default defineConfig({
   viewportHeight: 1080,
   viewportWidth: 1920,
   e2e: {
-    setupNodeEvents(on, config) {
+    setupNodeEvents(on) {
       configurePlugin(on);
     },
     specPattern: "cypress/e2e/**/*.spec.{js,jsx,ts,tsx}",
